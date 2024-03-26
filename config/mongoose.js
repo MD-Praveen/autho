1847b0c02a8ed8a9c85d63b1fba849a7b7ae90fc
MD-Praveen <161420275+MD-Praveen@users.noreply.github.com>
2024-03-26 13:05:53 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,17 +1,15 @@
+const link = 'mongodb://127.0.0.1:27017/myapp';
+
+
+
+
+
 const mongoose = require('mongoose');
 
-const uri = "mongodb+srv://sriharijagan333:o8J4s6T2i23my8q1@cluster0.2xscnr7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-
-mongoose.connect(uri);
-
+mongoose.connect(link);
 const db = mongoose.connection;
 
-db.on('error', (err) => {
-    console.error('MongoDB connection error:', err.message);
-});
-
-db.once('open', () => {
-    console.log('DB is connected successfully...');
-});
+db.on('error',(err) => {console.log(err)});
+db.on('open',() => {console.log("DB is connected Successfully...")});
 
 module.exports = db;
