69f1874bc2b91a3f5fa7edf47a711640d730f1ae
MD-Praveen <161420275+MD-Praveen@users.noreply.github.com>
2024-03-26 14:22:29 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,12 +1,13 @@
 const link = 'mongodb://localhost:27017/myapp';
 
+const uri = "mongodb+srv://sriharijagan333:o8J4s6T2i23my8q1@cluster0.2xscnr7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 
 
 
 const mongoose = require('mongoose');
 
-mongoose.connect(link);
+mongoose.connect(uri);
 const db = mongoose.connection;
 
 db.on('error',(err) => {console.log(err)});
