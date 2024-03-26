4a55bd2dc8642e53f79e305d9053ab3b871b33ed
MD-Praveen <161420275+MD-Praveen@users.noreply.github.com>
2024-03-26 13:16:52 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ const app = express();
 
 const layout = require('express-layouts');
 const db = require('./config/mongoose');        //for DataBase
-const session = require('express-session');     
+
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
+
 const cookieParser = require('cookie-parser');
 // Passport Authoritication 
 const passport = require('passport');
@@ -22,11 +25,17 @@ app.use(express.static('asserts'));
 app.use(cookieParser()); 
 app.use(express.urlencoded());
 
+const store = new MongoDBStore({
+    uri: 'mongodb://localhost:27017/myapp',
+    collection: 'sessions'
+});
+
 app.use(session({
     name: 'Myapp',
     secret: 'keyboard cat',
     resave: false,
     saveUninitialized: true,
+    store: store,
     cookie: {
         maxAge: 1000 * 60 * 10
     }
